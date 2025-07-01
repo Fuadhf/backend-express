17665a0ef1cc579335e8f5418fc59c0d3c136771
Fuadhf <hanipp959@gmail.com>
2025-07-01 13:43:44 +0700

    fix(route): add missing slash in /admin/user/:id path

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,8 @@ router.post('/register', validateRegister, registerController.register);
 router.post('/login', validateLogin, loginController.login);
 router.get('/admin/users', verifyToken, userController.findUser)
 router.post('/admin/user', verifyToken, validateUser, userController.createUser);
-router.get('/admin/user:id', verifyToken, userController.findUserById);
-router.put('/admin/user:id', verifyToken, validateUser, userController.updateUserById);
-router.delete('/admin/user:id', verifyToken, userController.deleteUser);
+router.get('/admin/user/:id', verifyToken, userController.findUserById);
+router.put('/admin/user/:id', verifyToken, validateUser, userController.updateUserById);
+router.delete('/admin/user/:id', verifyToken, userController.deleteUser);
 
 module.exports = router;
