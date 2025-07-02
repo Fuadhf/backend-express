0710b87b054a2c80dc6d9f9d83c7447edb39b2a2
Fuadhf <hanipp959@gmail.com>
2025-07-02 21:35:01 +0700

    Fix(port): add env port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const router = require('./routes/index')
 const cookieParser = require('cookie-parser')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors({
   origin: "https://admin.tirex.my.id",
